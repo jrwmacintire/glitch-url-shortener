89124329b07233c1653515ee2dbc48b8094a5284
Glitch (jrwm3-url-shortener) <none>
2018-12-08 17:39:02 +0000

    🐆🍨 Checkpoint ./server.js:1589149/611

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+const express = require('express');
+const mongo = require('mongodb');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-var cors = require('cors');
-
-var app = express();
+const app = express();
 
 // Basic Configuration 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.MONGOLAB_URI);
@@ -18,20 +18,31 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
   
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/hello", (req, res) => {
   res.json({greeting: 'hello API'});
 });
 
+// @route POST '/api/shorturl/new'
+// @desc Create a new shorturl using the POST request's query
+app.post('/api/shorturl/new',(req, res) => {
+  
+});
 
-app.listen(port, function () {
+
+app.listen(port, () => {
   console.log('URL Shortener is listening ...');
 });
